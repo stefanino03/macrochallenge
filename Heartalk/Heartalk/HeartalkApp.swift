015085140213634cdbb6588c2b06f05bbadd9c28
Stefano Giacobbe <sgiacobbe22@fed.idserve.net>
2023-06-09 16:45:22 +0200

    -first commit

---
+++ Heartalk/Heartalk/HeartalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartalkApp.swift
+//  Heartalk
+//
+//  Created by Stefano Giacobbe on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct HeartalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
