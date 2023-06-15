1c0526387a8be7d9f1e34a26f19b145d2f98b19f
Gregorio Guarino <113528856+GreGuarino66@users.noreply.github.com>
2023-06-15 14:02:05 +0200

    Progetto sbuggato

---
+++ Heartalk/HeartalkApp.swift
@@ -0,0 +1,20 @@
+//
+//  HeartalkApp.swift
+//  Heartalk
+//
+//  Created by Stefano Giacobbe on 09/06/23.
+//
+
+import SwiftUI
+
+@main
+struct HeartalkApp: App
+    {
+        var body: some Scene
+            {
+                WindowGroup
+                    {
+                        ContentView()
+                    }
+            }
+    }
