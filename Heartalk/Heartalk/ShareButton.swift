af05ee2253af0b716b7f2c452d8191f5ac5397ce
Stefano Giacobbe <sgiacobbe22@fed.idserve.net>
2023-06-15 14:40:23 +0200

    -added Quote model

--- Heartalk/Heartalk/ShareButton.swift
+++ Heartalk/Heartalk/ShareButton.swift
@@ -22,20 +22,6 @@ struct ShareButton: View
             }
     }
 
-struct Quote: Identifiable
-    {
-        var id = UUID()
-        var text: String
-    }
-
-extension Quote: Transferable
-    {
-        static var transferRepresentation: some TransferRepresentation
-            {
-                ProxyRepresentation(exporting: \.text)
-            }
-    }
-
 struct ShareButton_Previews: PreviewProvider
     {
         static var previews: some View
